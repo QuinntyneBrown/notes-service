fd701d5144a6e078a47ffade2525469914ca95b6
Quinntyne Brown <quinntynebrown@gmail.com>
2017-09-18 18:08:12 -0400

    set Tenant

---
+++ Features/Tenants/SetTenantCommand.cs
@@ -0,0 +1,70 @@
+using MediatR;
+using NotesService.Data;
+using NotesService.Features.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data.Entity;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace NotesService.Features.Tenants
+{
+    public class TenantExistsResponse
+    {
+        public bool Exists { get; set; }
+    }
+
+    public class SetTenantCommand
+    {
+        public class Request : BaseRequest, IRequest<Response>
+        {
+            public Guid UniqueId { get; set; }
+        }
+
+        public class Response { }
+
+        public class Handler : IAsyncRequestHandler<Request, Response>
+        {
+            public Handler(NotesServiceContext context, HttpClient client)
+            {
+                _context = context;
+                _client = client;
+            }
+
+            public async Task<Response> Handle(Request request)
+            {
+                var tenant = await _context.Tenants.SingleOrDefaultAsync(x => x.UniqueId == request.TenantUniqueId);
+
+                if (tenant != null)
+                    return new Response();
+
+                var httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("http://localhost:62233/api/tenants/exists"),
+                    Method = HttpMethod.Get,
+                };
+
+                httpRequestMessage.Headers.Add("Tenant", $"{request.UniqueId}");
+                
+                var httpResponseMessage = await _client.SendAsync(httpRequestMessage);
+                var response = await httpResponseMessage.Content.ReadAsAsync<TenantExistsResponse>();
+                
+                if(response.Exists)
+                {
+                    tenant = new Model.Tenant() { UniqueId = request.UniqueId, Name = $"{request.UniqueId}" };
+                    _context.Tenants.Add(tenant);
+                    await _context.SaveChangesAsync();
+                    return new Response();
+                }
+
+                throw new Exception("Tenant doesn't exist");
+            }
+
+            private readonly NotesServiceContext _context;            
+            private readonly HttpClient _client;
+        }
+
+    }
+}
